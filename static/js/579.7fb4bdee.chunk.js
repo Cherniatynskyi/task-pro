6207f341dfb32a45fbb96d665e0a2edeb578685a
Cherniatynskyi <87269655+Cherniatynskyi@users.noreply.github.com>
2024-03-02 15:30:57 +0000

    Deploying to gh-pages from @ Cherniatynskyi/task-pro@60d1f5ae5bbc3ff2b193b867aea36073aa02b365 🚀

--- static/js/579.7fb4bdee.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktask_pro=self.webpackChunktask_pro||[]).push([[579],{960:(s,t,a)=>{a.r(t),a.d(t,{default:()=>k});const e={};var c=a(579);const k=()=>(0,c.jsx)("div",{className:e.test})}}]);
-//# sourceMappingURL=579.7fb4bdee.chunk.js.map
